87f9a10314019de16cbd85038f7bcb684d060a3f
Tabrezsheik <133689080+Tabrezsheik@users.noreply.github.com>
2026-06-01 16:57:03 +0530

    Delete JavaScript/Variables,Conditionals directory

--- JavaScript/Variables,Conditionals/Even&Odd.js
+++
@@ -1,7 +0,0 @@
-let num = parseInt(prompt("Enter a number: "));
-
-if (num % 2 === 0) {
-    console.log(`${num} is an even number.`);
-} else {
-    console.log(`${num} is an odd number.`);
-}
--- JavaScript/Variables,Conditionals/Num Type Checker.js
+++
@@ -1,8 +0,0 @@
-let num = parseInt(prompt("Enter a number:"));
-if(num > 0 ) {
-    console.log(`${num} is a positive number.`);
-} else if(num < 0) {
-    console.log(`${num} is a negative number.`);
-} else {
-    console.log(`${num} is zero.`);
-}
