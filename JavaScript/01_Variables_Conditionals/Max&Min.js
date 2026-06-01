afa150e02772f095ab49e20c95e3d07702411b07
Tabrezsheik <133689080+Tabrezsheik@users.noreply.github.com>
2026-06-01 21:49:56 +0530

    Update Max&Min.js

--- JavaScript/01_Variables_Conditionals/Max&Min.js
+++ JavaScript/01_Variables_Conditionals/Max&Min.js
@@ -1,9 +1,24 @@
-const a = 20;
-const b = 30;
-const c = 15;
-
-const max = Math.max(a, b, c);
-const min = Math.min(a, b, c);
-
-console.log(`Maximum: ${max}`);
-console.log(`Minimum: ${min}`);
+let num1 = parseInt(prompt("enter number1: "));
+let num2 = parseInt(prompt("enter number2: "));
+let num3 = parseInt(prompt("enter number3: "));
+//max logic
+if(num1 > num2 && num1 > num3){
+    console.log(`${num1} is a max`);
+}
+else if(num2 > num3){
+    console.log(`${num2} is a max`);
+    }
+else{
+    console.log(`${num3} is a max`);
+}
+//min logic
+if(num1 < num2 && num1 < num3){
+    console.log(`${num1} is a min`);
+}
+else if(num2 < num3){
+    console.log(`${num2} is a min`);
+    }
+else{
+    console.log(`${num3} is a min`);
+}
+//can also use Math.max(a,b,c), Math.min(a,b,c)
