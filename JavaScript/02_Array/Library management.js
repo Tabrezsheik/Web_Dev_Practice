5d259950acd119461f04cead306bfa876f70020a
Tabrezsheik <133689080+Tabrezsheik@users.noreply.github.com>
2026-06-02 21:25:46 +0530

    Update Library management.js

--- JavaScript/02_Array/Library management.js
+++ JavaScript/02_Array/Library management.js
@@ -1,7 +1,26 @@
-const library = [
-  { id: 1, title: 'JavaScript Basics', author: 'John Doe' },
-  { id: 2, title: 'Advanced JS', author: 'Jane Smith' },
-  { id: 3, title: 'Web Dev Guide', author: 'Bob Johnson' }
-];
+//  Library Book System
+// Features:
+// Add books
+// Remove books
+// Check if book exists
+// Show all books give in steps
+let books = ["Harry Potter", "Atomic Habits", "Rich Dad Poor Dad"];
 
-console.log(library);
+// Show books
+console.log(books);
+
+// Add book at the end
+books.push("The Alchemist");
+console.log(books);
+
+// Remove last book
+books.pop();
+console.log(books);
+
+// Check book exists
+console.log(books.includes("Atomic Habits"));
+
+console.log(books.includes("The Alchemist"));
+
+// Show final books
+console.log(books);
