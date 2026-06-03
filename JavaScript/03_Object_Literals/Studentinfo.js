0825865d54cc79cc3eef2b31c5c00d84be255572
Tabrezsheik <133689080+Tabrezsheik@users.noreply.github.com>
2026-06-03 21:02:53 +0530

    Modify student object with new details and marks
    Updated student information and added marks for subjects.

--- JavaScript/03_Object_Literals/Studentinfo.js
+++ JavaScript/03_Object_Literals/Studentinfo.js
@@ -1,12 +1,37 @@
 const student = {
-  name: 'John',
+  name: "Tabrez",
   age: 20,
-  gpa: 3.7,
-  major: 'Computer Science',
-  courses: ['JavaScript', 'Python', 'Data Structures'],
-  info: function() {
-    console.log(`${this.name} is a ${this.age} year old ${this.major} student`);
+  city: "Ananthapur",
+
+  marks: {
+    maths: 80,
+    science: 75
   }
 };
 
-student.info();
+// Print normal values
+console.log(`${student["name"]} is ${student["age"]} years old and lives in ${student["city"]}.`)
+
+
+
+// Add new subject
+// Print nested object values
+console.log(`Maths Marks: ${student.marks.maths}`);
+console.log(`Science Marks: ${student["marks"]["science"]}`);
+
+student.marks.english = 85;
+// Update maths marks
+student.marks.maths = 90;
+// add new subject
+student.marks["history"] = 70;
+
+// Print the entire marks object
+ console.log(student.marks);
+ // Calculate total marks
+let total =
+  student.marks.maths +
+  student.marks.science +
+  student.marks.english + student.marks.history;
+ 
+
+console.log(`Total Marks: ${total}`);
