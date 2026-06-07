bf564268102e8b3b7f8cab5553d8235e6afb4d69
Tabrezsheik <133689080+Tabrezsheik@users.noreply.github.com>
2026-06-07 16:27:06 +0530

    Implement string capitalization function
    Added a function to capitalize the first letter of a string and updated console logs accordingly.

--- JavaScript/05_Basic_Function/fun1task.js
+++ JavaScript/05_Basic_Function/fun1task.js
@@ -1,5 +1,6 @@
-function greet(name) {
-  return `Hello, ${name}!`;
+function test(str){
+    return str[0].toUpperCase()+str.slice(1,str.length);
 }
-
-console.log(greet('World'));
+console.log(test("hello"));//makes the first letter of the string uppercase and rest of the letters remain unchanged.
+x = test(prompt("Enter a string:"));//takes input from the user and applies the same function to it.
+console.log(x);
