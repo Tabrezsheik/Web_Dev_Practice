20159064cc483571c8977a0daa1039753458b6ca
Tabrezsheik <133689080+Tabrezsheik@users.noreply.github.com>
2026-06-07 16:23:03 +0530

    Refactor calculateMarks function and add prompts

--- JavaScript/05_Basic_Function/CalculateMarks.js
+++ JavaScript/05_Basic_Function/CalculateMarks.js
@@ -1,6 +1,18 @@
-function calculateMarks(subject1, subject2, subject3) {
-  return (subject1 + subject2 + subject3) / 3;
-}
+// function CalculateMarks(engMarks, mathMarks, scienceMarks) {
+//     let totalMarks = 0;
+//     totalMarks = engMarks + mathMarks + scienceMarks;
+//     let avgMarks = totalMarks / 3;
+//     return  {total: totalMarks, average: avgMarks};
+// }
+// let x = CalculateMarks(85, 90, 78);//sample marks for English, Math and Science
+// console.log(`Total Marks :${x.total} \nAverage Marks :${x.average.toFixed(2)}`);
 
-const average = calculateMarks(85, 90, 78);
-console.log(`Average marks: ${average}`);
+
+
+function CalculateMarks(e, m, s) {
+    let totalMarks = e + m + s;
+    let avgMarks = totalMarks / 3;
+    return { total: totalMarks, average: avgMarks };
+}
+let x = CalculateMarks(parseInt(prompt("Enter English marks:")), parseInt(prompt("Enter Math marks:")), parseInt(prompt("Enter Science marks:")));//sample marks for English, Math and Science
+console.log(`Total Marks :${x.total} \nAverage Marks :${x.average.toFixed(2)}`);
