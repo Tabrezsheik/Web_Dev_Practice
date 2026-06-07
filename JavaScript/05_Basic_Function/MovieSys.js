2e5689810cd129132ed235365139be4ced63f1cd
Tabrezsheik <133689080+Tabrezsheik@users.noreply.github.com>
2026-06-07 16:32:52 +0530

    Refactor movie data and add highest rated movie function
    Refactored movie data structure and added a function to find the highest rated movie.

--- JavaScript/05_Basic_Function/MovieSys.js
+++ JavaScript/05_Basic_Function/MovieSys.js
@@ -1,10 +1,40 @@
-const movies = [
-  { title: 'Inception', rating: 8.8, year: 2010 },
-  { title: 'The Matrix', rating: 8.7, year: 1999 }
-];
-
-function findMovieByRating(minRating) {
-  return movies.filter(movie => movie.rating >= minRating);
+function movies() {
+    const movies = [
+        {
+            name: "The Dark Knight",
+            rating: 9.0,
+            genre: "Action"
+        },
+        {
+            name: "Avengers: Endgame",
+            rating: 8.9,
+            genre: "Action"
+        },
+        {
+            name: "The Shawshank Redemption",
+            rating: 9.3,
+            genre: "Drama"
+        },
+        {
+            name: "The Lord of the Rings: The Return of the King",
+            rating: 8.9,
+            genre: "Fantasy"
+        }
+    ];
+    // returing highest rated movie
+    let maxrate = 0;
+    let ratedmovie = {};
+    for (let i = 0; i < movies.length; i++) {
+        if (movies[i].rating > maxrate) {
+            maxrate = movies[i].rating;
+            ratedmovie = {
+                name: movies[i].name,
+                rating: movies[i].rating,
+                genre: movies[i].genre
+            }
+        }
+    }
+    return ratedmovie;
 }
 
-console.log(findMovieByRating(8.7));
+console.log(movies());
