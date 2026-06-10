f9a007a2bc1a0398b6be0cea9c0c7976d86cfb59
Tabrezsheik <133689080+Tabrezsheik@users.noreply.github.com>
2026-06-10 11:54:16 +0530

    Update Set Time&Interval.js

--- JavaScript/06_Array_Callbacks/Set Time&Interval.js
+++ JavaScript/06_Array_Callbacks/Set Time&Interval.js
@@ -1,10 +1,15 @@
 setTimeout(() => {
-  console.log('This runs after 2 seconds');
-}, 2000);
+    console.log("Hello My name is setTimeout");
+}, 2000);// here we are passing a function as the first argument to setTimeout, which will be executed after 2000 milliseconds (2 seconds). The second argument is the delay time in milliseconds.
 
-let count = 0;
-const interval = setInterval(() => {
-  count++;
-  console.log(`Count: ${count}`);
-  if (count === 5) clearInterval(interval);
-}, 1000);
+setTimeout((num) => {
+    console.log(num += 200);
+}, 3000, 100);// here we are passing a function as the first argument to setTimeout, which will be executed after 3000 milliseconds (3 seconds). The second argument is the delay time in milliseconds, and the third argument (100) is passed as an argument to the callback function when it is executed.
+
+const intervalId = setInterval(() => {
+    console.log("Hello My name is setInterval");
+}, 1000);// here we are passing a function as the first argument to setInterval, which will be executed every 1000 milliseconds (1 second). The second argument is the interval time in milliseconds.
+
+setTimeout(() => {
+    clearInterval(intervalId);// here we are using clearInterval to stop the interval after 5000 milliseconds (5 seconds).
+}, 5000);
