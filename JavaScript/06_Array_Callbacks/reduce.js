7cb27ba221d001428bf111d13835548543b648a1
Tabrezsheik <133689080+Tabrezsheik@users.noreply.github.com>
2026-06-10 12:11:47 +0530

    Update reduce.js

--- JavaScript/06_Array_Callbacks/reduce.js
+++ JavaScript/06_Array_Callbacks/reduce.js
@@ -1,7 +1,16 @@
-const numbers = [1, 2, 3, 4, 5];
+// reduce() is a powerful method in JavaScript that allows you to reduce an array to a single value by applying a function to each element of the array.
+//  In this example, we will use reduce() to find the longest word from an array of words entered by the user.
 
-const sum = numbers.reduce((acc, num) => acc + num, 0);
-const product = numbers.reduce((acc, num) => acc * num, 1);
-
-console.log('Sum:', sum);
-console.log('Product:', product);}
+let WordsArray = [];
+for(i=0;i<5;i++){
+    let word = prompt("enter a word:");
+    WordsArray.push(word);
+}
+const longestWord = WordsArray.reduce((longest,currentword) => {
+    if (currentword.length > longest.length) {
+        return currentword;
+    }
+    
+        return longest;
+})
+console.log(`The longest word is: ${longestWord}`);
