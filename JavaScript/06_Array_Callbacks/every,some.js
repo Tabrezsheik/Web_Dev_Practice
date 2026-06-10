9e9708bb320cdf02a55256b28a29280be08bb36a
Tabrezsheik <133689080+Tabrezsheik@users.noreply.github.com>
2026-06-10 11:57:28 +0530

    Update every,some.js

--- JavaScript/06_Array_Callbacks/every,some.js
+++ JavaScript/06_Array_Callbacks/every,some.js
@@ -1,8 +1,32 @@
-const numbers = [2, 4, 6, 8];
-const numbers2 = [1, 2, 3, 5];
+const StudentsOfA = [
+  { name: 'Alice', grade: 85 },
+  { name: 'Bob', grade: 92 },
+  { name: 'Charlie', grade: 72 },
+  { name: 'David', grade: 90 },
+];
 
-const allEven = numbers.every(num => num % 2 === 0);
-const hasOdd = numbers2.some(num => num % 2 !== 0);
+// Check if every student has a grade above 80
+const allAbove80 = StudentsOfA.every(student => student.grade > 80);
+console.log(`All students of Class A have a grade above 80: ${allAbove80}`);// output: false
 
-console.log('All even:', allEven);
-console.log('Has odd:', hasOdd);
+const StudentsOfB = [
+  { name: 'Eve', grade: 90 },
+  { name: 'Frank', grade: 88 },
+  { name: 'Grace', grade: 95 },
+  { name: 'Heidi', grade: 91 },
+];
+// Check if some students have a grade above 80
+const someAbove80 = StudentsOfB.every(student => student.grade > 80);
+console.log(`All students of Class B have a grade above 80: ${someAbove80}`);// output: true
+
+
+//example of some
+const StudentsOfC = [
+  { name: 'Ivan', grade: 75 },
+    { name: 'Judy', grade: 82 },
+    { name: 'Karl', grade: 78 },
+    { name: 'Leo', grade: 80 },
+];
+// Check if some students have a grade above 80
+const someAbove80InC = StudentsOfC.some(student => student.grade > 80);
+console.log(`Some students of Class C have a grade above 80: ${someAbove80InC}`); // output: true
